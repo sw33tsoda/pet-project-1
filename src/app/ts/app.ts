9b0f1317d7a0923bd16ce70030dd3ef025a5bff9
sw33tsoda <sw33tsoda@gmail.com>
2021-04-26 07:15:33 +0700

    update

--- src/app/ts/app.ts
+++ src/app/ts/app.ts
@@ -102,6 +102,156 @@ class Navbar {
 
 // =================================================================== //
 
+
+
+interface Job {
+    title?:string,
+    position?:string,
+    year_exp_required?:number,
+    quantity?:number,
+}
+
+class JobsPagination {
+    private list:Array<Job> = [];
+    private pagination = {
+        currentPage:0,
+        maxItems:0,
+        itemsPerPage:6,
+        maxPages:0,
+    };
+
+    constructor() {
+        for (const direction of ['prev','next']) {
+            $(`.career__available-jobs__controls__${direction}`).addEventListener('click', () => this.paginate(direction));
+        }
+    }
+
+    public paginate = (direction:string) : void => {
+        const lastPageIndex = this.pagination.currentPage;
+        switch (direction) {
+            case 'prev' : {
+                if (this.pagination.currentPage > 0) {
+                    this.pagination.currentPage -=1;
+                }
+                break;
+            }
+
+            case 'next' : {
+                if (this.pagination.currentPage < this.pagination.maxPages - 1) {
+                    this.pagination.currentPage +=1;
+                }
+                break;
+            }
+            default:break;
+        }
+        if (lastPageIndex !== this.pagination.currentPage) {
+            this.render();
+        }
+    }
+    
+    public setList = (list:Array<Job>) : JobsPagination => {
+        this.list = list;
+        this.pagination.maxItems = list.length;
+        this.pagination.maxPages = Math.round(list.length / this.pagination.itemsPerPage);
+
+        return this;
+    }
+
+    public getList = () : Array<Job> => {
+        return this.list;
+    }
+
+    public render = () : void => {
+        console.log(this.pagination);
+        $('.career__available-jobs__controls__current').innerText = this.pagination.currentPage + 1 + '';
+        let HTML = ``;
+        const from = this.pagination.currentPage * this.pagination.itemsPerPage;
+        const to = from + this.pagination.itemsPerPage;
+        const renderList = this.list.slice(from,to);
+        console.log(renderList,from,to);
+        for (const item of renderList) {
+            HTML += `
+                <div class='career__available-jobs__list__job'>
+                    <div class='career__available-jobs__list__job__title'>${item.title}</div>
+                    <div class='career__available-jobs__list__job__position'>${item.position}</div>
+                    <div class='career__available-jobs__list__job__year-exp'>${item.year_exp_required}年</div>
+                    <div class='career__available-jobs__list__job__quantity'>${item.quantity} <i class="fas fa-user-friends"></i></div>
+                </div>
+            `;
+        }
+        $('.career__available-jobs__list').innerHTML = HTML;
+    }
+}
+
+interface Slide {
+    imageUrl?:string,
+    title?:string,
+    description?:string,
+}
+
+class Slider {
+    private list:Array<Slide> = [];
+    private currentSlide = 0;
+    private delay = 3000;
+
+    constructor() {
+        
+    }
+    
+    public setList = (list:Array<Slide>) : Slider => {
+        this.list = list;
+        if (this.list.length > 0) {
+            setInterval(() => {
+                if (this.currentSlide < this.list.length - 1) {
+                    this.currentSlide += 1;
+                } else {
+                    this.currentSlide = 0;
+                }
+                this.setCurrentSlide(this.currentSlide);
+    
+            },this.delay);
+        }
+        this.renderSliderControls();
+        return this;
+    }
+
+    public getList = () : Array<Slide> => {
+        return this.list;
+    }
+
+    public setCurrentSlide = (index:number) : void => {
+        this.currentSlide = index;
+        this.renderSliderControls();
+        this.renderSlide();
+    }
+
+    public renderSliderControls = () : void => {
+        let HTML = '';
+        for (let index = 0; index < this.list.length; index++) {
+            HTML += `<div class="slider__controls__switch ${this.currentSlide == index && 'slider__controls__switch--selected'}" onclick='_Slider.setCurrentSlide(${index})'></div>`
+        }
+        // console.log(HTML);
+        $('.slider__controls').innerHTML = HTML;
+    }
+
+    public renderSlide = () : void => {
+        $('.slider__slide-show').innerHTML = `
+            <div class='slider__slide-show__slide'>
+                <div class='slider__slide-show__slide__left'>
+                    <div class='slider__slide-show__slide__left__title'>${this.list[this.currentSlide].title}</div>
+                    <div class='slider__slide-show__slide__left__description'>${this.list[this.currentSlide].description}</div>
+                    <div class='slider__slide-show__slide__left__button'>${this.list[this.currentSlide].description}</div>
+                </div>
+                <div class='slider__slide-show__slide__right'>
+                    <div class='slider__slide-show__slide__right__image'>
+                        <img src='${this.list[this.currentSlide].imageUrl}'/>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
 // Event.
 
 
@@ -124,11 +274,15 @@ class App {
                 document.querySelectorAll('.form-control__input')[index].previousElementSibling.classList.add('form-control__label--has-value');
             });
             document.querySelectorAll('.form-control__input')[index].addEventListener('blur',() => {
-                if (document.querySelectorAll('.form-control__input')[index].value == '') {
+                if (document.querySelectorAll<HTMLInputElement>('.form-control__input')[index].value == '') {
                     document.querySelectorAll('.form-control__input')[index].previousElementSibling.classList.remove('form-control__label--has-value');
                 }
             });
         }
+
+        $('#job-form').addEventListener('submit', (event) => {
+            event.preventDefault();
+        });
     }
     
     public main() { 
@@ -138,8 +292,30 @@ class App {
             .setBlock('navbar')
             .setEle('navbar__list')
             .useScrollToElement(['#_homepage','#_services','#_our-team','#_career','#_contact']);
+
+        new JobsPagination().setList([
+            {title:'野無氏遊',position:'むお保',year_exp_required:1,quantity:5},
+            {title:'ひほは',position:'課御',year_exp_required:2,quantity:10},
+            {title:'課舳',position:'巣雲日手',year_exp_required:1,quantity:6},
+            {title:'都模毛',position:'夜ミマ',year_exp_required:1,quantity:2},
+            {title:'ホネル保',position:'舳巣等し',year_exp_required:1,quantity:1},
+            {title:'あゃナカ',position:'樹都模毛',year_exp_required:0,quantity:20},
+            {title:'ぬへ無',position:'ら根絵ス',year_exp_required:3,quantity:5},
+            {title:'無瀬列',position:'い根舳',year_exp_required:1,quantity:3},
+            {title:'ま以保絵',position:'てす留個て',year_exp_required:1,quantity:6},
+            {title:'う津派まそ',position:'瀬絵むし',year_exp_required:1,quantity:8},
+            {title:'根舳のま',position:'雲舳知',year_exp_required:1,quantity:10},
+        ]).render();
+
     }
 }
+
+const _Slider = new Slider();
+_Slider.setList([
+    {imageUrl:'',title:'title1',description:'desc1'},
+    {imageUrl:'',title:'title2',description:'desc2'},
+    {imageUrl:'',title:'title3',description:'desc3'},
+]);
 
 const app = new App().main();
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -76,6 +76,113 @@ var Navbar = (function () {
     }
     return Navbar;
 }());
+var JobsPagination = (function () {
+    function JobsPagination() {
+        var _this = this;
+        this.list = [];
+        this.pagination = {
+            currentPage: 0,
+            maxItems: 0,
+            itemsPerPage: 6,
+            maxPages: 0
+        };
+        this.paginate = function (direction) {
+            var lastPageIndex = _this.pagination.currentPage;
+            switch (direction) {
+                case 'prev': {
+                    if (_this.pagination.currentPage > 0) {
+                        _this.pagination.currentPage -= 1;
+                    }
+                    break;
+                }
+                case 'next': {
+                    if (_this.pagination.currentPage < _this.pagination.maxPages - 1) {
+                        _this.pagination.currentPage += 1;
+                    }
+                    break;
+                }
+                default: break;
+            }
+            if (lastPageIndex !== _this.pagination.currentPage) {
+                _this.render();
+            }
+        };
+        this.setList = function (list) {
+            _this.list = list;
+            _this.pagination.maxItems = list.length;
+            _this.pagination.maxPages = Math.round(list.length / _this.pagination.itemsPerPage);
+            return _this;
+        };
+        this.getList = function () {
+            return _this.list;
+        };
+        this.render = function () {
+            console.log(_this.pagination);
+            $('.career__available-jobs__controls__current').innerText = _this.pagination.currentPage + 1 + '';
+            var HTML = "";
+            var from = _this.pagination.currentPage * _this.pagination.itemsPerPage;
+            var to = from + _this.pagination.itemsPerPage;
+            var renderList = _this.list.slice(from, to);
+            console.log(renderList, from, to);
+            for (var _i = 0, renderList_1 = renderList; _i < renderList_1.length; _i++) {
+                var item = renderList_1[_i];
+                HTML += "\n                <div class='career__available-jobs__list__job'>\n                    <div class='career__available-jobs__list__job__title'>" + item.title + "</div>\n                    <div class='career__available-jobs__list__job__position'>" + item.position + "</div>\n                    <div class='career__available-jobs__list__job__year-exp'>" + item.year_exp_required + "\u5E74</div>\n                    <div class='career__available-jobs__list__job__quantity'>" + item.quantity + " <i class=\"fas fa-user-friends\"></i></div>\n                </div>\n            ";
+            }
+            $('.career__available-jobs__list').innerHTML = HTML;
+        };
+        var _loop_1 = function (direction) {
+            $(".career__available-jobs__controls__" + direction).addEventListener('click', function () { return _this.paginate(direction); });
+        };
+        for (var _i = 0, _a = ['prev', 'next']; _i < _a.length; _i++) {
+            var direction = _a[_i];
+            _loop_1(direction);
+        }
+    }
+    return JobsPagination;
+}());
+var Slider = (function () {
+    function Slider() {
+        var _this = this;
+        this.list = [];
+        this.currentSlide = 0;
+        this.delay = 3000;
+        this.setList = function (list) {
+            _this.list = list;
+            if (_this.list.length > 0) {
+                setInterval(function () {
+                    if (_this.currentSlide < _this.list.length - 1) {
+                        _this.currentSlide += 1;
+                    }
+                    else {
+                        _this.currentSlide = 0;
+                    }
+                    _this.setCurrentSlide(_this.currentSlide);
+                }, _this.delay);
+            }
+            _this.renderSliderControls();
+            return _this;
+        };
+        this.getList = function () {
+            return _this.list;
+        };
+        this.setCurrentSlide = function (index) {
+            _this.currentSlide = index;
+            _this.renderSliderControls();
+            _this.renderSlide();
+        };
+        this.renderSliderControls = function () {
+            var HTML = '';
+            for (var index = 0; index < _this.list.length; index++) {
+                HTML += "<div class=\"slider__controls__switch " + (_this.currentSlide == index && 'slider__controls__switch--selected') + "\" onclick='_Slider.setCurrentSlide(" + index + ")'></div>";
+            }
+            $('.slider__controls').innerHTML = HTML;
+        };
+        this.renderSlide = function () {
+            $('.slider__slide-show').innerHTML = "\n            <div class='slider__slide-show__slide'>\n                <div class='slider__slide-show__slide__left'>\n                    <div class='slider__slide-show__slide__left__title'>" + _this.list[_this.currentSlide].title + "</div>\n                    <div class='slider__slide-show__slide__left__description'>" + _this.list[_this.currentSlide].description + "</div>\n                    <div class='slider__slide-show__slide__left__button'>" + _this.list[_this.currentSlide].description + "</div>\n                </div>\n                <div class='slider__slide-show__slide__right'>\n                    <div class='slider__slide-show__slide__right__image'>\n                        <img src='" + _this.list[_this.currentSlide].imageUrl + "'/>\n                    </div>\n                </div>\n            </div>\n        ";
+        };
+    }
+    return Slider;
+}());
 var App = (function () {
     function App() {
         window.onload = function () {
@@ -88,7 +195,7 @@ var App = (function () {
                 $('.header').classList.remove('breakline');
             }
         });
-        var _loop_1 = function (index) {
+        var _loop_2 = function (index) {
             document.querySelectorAll('.form-control__input')[index].addEventListener('focus', function () {
                 document.querySelectorAll('.form-control__input')[index].previousElementSibling.classList.add('form-control__label--has-value');
             });
@@ -99,8 +206,11 @@ var App = (function () {
             });
         };
         for (var index = 0; index < document.querySelectorAll('.form-control__input').length; index++) {
-            _loop_1(index);
+            _loop_2(index);
         }
+        $('#job-form').addEventListener('submit', function (event) {
+            event.preventDefault();
+        });
     }
     App.prototype.main = function () {
         new Navbar()
@@ -109,7 +219,26 @@ var App = (function () {
             .setBlock('navbar')
             .setEle('navbar__list')
             .useScrollToElement(['#_homepage', '#_services', '#_our-team', '#_career', '#_contact']);
+        new JobsPagination().setList([
+            { title: '野無氏遊', position: 'むお保', year_exp_required: 1, quantity: 5 },
+            { title: 'ひほは', position: '課御', year_exp_required: 2, quantity: 10 },
+            { title: '課舳', position: '巣雲日手', year_exp_required: 1, quantity: 6 },
+            { title: '都模毛', position: '夜ミマ', year_exp_required: 1, quantity: 2 },
+            { title: 'ホネル保', position: '舳巣等し', year_exp_required: 1, quantity: 1 },
+            { title: 'あゃナカ', position: '樹都模毛', year_exp_required: 0, quantity: 20 },
+            { title: 'ぬへ無', position: 'ら根絵ス', year_exp_required: 3, quantity: 5 },
+            { title: '無瀬列', position: 'い根舳', year_exp_required: 1, quantity: 3 },
+            { title: 'ま以保絵', position: 'てす留個て', year_exp_required: 1, quantity: 6 },
+            { title: 'う津派まそ', position: '瀬絵むし', year_exp_required: 1, quantity: 8 },
+            { title: '根舳のま', position: '雲舳知', year_exp_required: 1, quantity: 10 },
+        ]).render();
     };
     return App;
 }());
+var _Slider = new Slider();
+_Slider.setList([
+    { imageUrl: '', title: 'title1', description: 'desc1' },
+    { imageUrl: '', title: 'title2', description: 'desc2' },
+    { imageUrl: '', title: 'title3', description: 'desc3' },
+]);
 var app = new App().main();
